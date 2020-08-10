5814c3af827185091218a28ad60258669e3c555a
Elena Bravo <hey@elenabravo.com>
2020-08-10 15:58:46 +0200

    add some fixes and refractor webpack config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,21 +1,22 @@
 const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
-module.exports = (env, options) =>
-  merge(baseConfig(options.mode), {
-    mode: 'development',
-    devServer: {
-      port: 3000,
-      open: true,
-      overlay: {
-        warnings: true,
-        errors: true
-      },
-      historyApiFallback: true,
-      hot: true
+const mode = 'development'
+
+module.exports = merge(baseConfig(mode), {
+  mode,
+  devServer: {
+    port: 3000,
+    open: true,
+    overlay: {
+      warnings: true,
+      errors: true
     },
-    devtool: 'source-map',
-    resolve: {
-      extensions: ['.js', '.sass']
-    }
-  })
+    historyApiFallback: true,
+    hot: true
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.css', '.sass', '.scss']
+  }
+})
